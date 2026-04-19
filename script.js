9d02e7855cf7dbc13b82c60007a89c96e1751b78
mohammedmajjati12 <mohammed.majjati12@gmail.com>
2026-04-19 13:54:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ async function func() {
 
     chat.scrollTop = chat.scrollHeight;
 
-    const response = await fetch("https://ai-chat-gx6hwa-production.up.railway.app/chat", {
+    const response = await fetch("http://127.0.0.1:8080/chat", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
