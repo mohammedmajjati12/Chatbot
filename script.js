be0681b84fb55e36cf1c2b972dc505f2c4c28261
mohammedmajjati12 <mohammed.majjati12@gmail.com>
2026-04-19 13:26:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+async function func() {
+    const input = document.getElementById("PW");
+    const pw = input.value;
+
+    const myMsg = document.createElement("div");
+    myMsg.className = "msg me";
+    myMsg.innerText = pw;
+    chat.appendChild(myMsg);
+
+    input.value = "";
+
+    const typingMsg = document.createElement("div");
+    typingMsg.className = "msg bot";
+    typingMsg.innerText = "écrire...";
+    chat.appendChild(typingMsg);
+
+    chat.scrollTop = chat.scrollHeight;
+
+    const response = await fetch("http://127.0.0.1:3000/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ pw })
+    });
+
+    const data = await response.json();
+
+    typingMsg.innerText = data.res;
+
+    chat.scrollTop = chat.scrollHeight;
+  }
